620d3f0d6505f38e33525050c5a287d54edf19ec
NataFomchenkova <43876309+NataFomchenkova@users.noreply.github.com>
2018-12-23 16:12:15 +0300

    Update heap.h

--- heap.h
+++ heap.h
@@ -12,28 +12,26 @@ class Heap
 {
 	int *elements; 
 	size_t size; 
-	int get_index(int elem); // получение индекса элемента по ключу 
-	void print_rec(int len, int x, int y, int cur); // вспомогательная для вывода 
-	void sorting(int index); // сортировка
+	int get_index(int elem); // РїРѕР»СѓС‡РµРЅРёРµ РёРЅРґРµРєСЃР° СЌР»РµРјРµРЅС‚Р° РїРѕ РєР»СЋС‡Сѓ 
+	void print_rec(int len, int x, int y, int cur); // РІСЃРїРѕРјРѕРіР°С‚РµР»СЊРЅР°СЏ РґР»СЏ РІС‹РІРѕРґР° 
+	void sorting(int index); // СЃРѕСЂС‚РёСЂРѕРІРєР°
 
 
 public:
 	Heap();
 	~Heap();
-	bool contains(int elem);  // поиск элемента в дереве по ключу
-	void insert(int elem);  // добавление элемента в дерево по ключу
-	void remove(int elem); // удаление элемента дерева по ключу
+	bool contains(int elem);  // РїРѕРёСЃРє СЌР»РµРјРµРЅС‚Р° РІ РґРµСЂРµРІРµ РїРѕ РєР»СЋС‡Сѓ
+	void insert(int elem);  // РґРѕР±Р°РІР»РµРЅРёРµ СЌР»РµРјРµРЅС‚Р° РІ РґРµСЂРµРІРѕ РїРѕ РєР»СЋС‡Сѓ
+	void remove(int elem); // СѓРґР°Р»РµРЅРёРµ СЌР»РµРјРµРЅС‚Р° РґРµСЂРµРІР° РїРѕ РєР»СЋС‡Сѓ
 	size_t get_size();
-	int at(size_t  index); // получение элемента по индексу
-	void printf();
-
+	int at(size_t  index); // РїРѕР»СѓС‡РµРЅРёРµ СЌР»РµРјРµРЅС‚Р° РїРѕ РёРЅРґРµРєСЃСѓ
+	
 
 	Iterator* create_width_iterator(); 
 	Iterator* create_depth_iterator();
 
-	void print_complete(); // вывод дерева на экран 
+	void print_complete(); // РІС‹РІРѕРґ РґРµСЂРµРІР° РЅР° СЌРєСЂР°РЅ 
 
-	//bool is_equal(int *array1, const size_t size_array1, int *array2, const size_t size_array2);
-
+	
 
 };
